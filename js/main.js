2a6f0496a5efaf597e44d871e6cf99ad4bcb2fc3
emunkhsaikhan <emonkhsaikhan@gmail.com>
2023-10-23 17:55:25 +0800

    slider nemev

--- js/main.js
+++ js/main.js
@@ -6,3 +6,33 @@ document.addEventListener('scroll', () => {
         navbar.style.background = 'transparent';
     }
 })
+
+const slides = document.querySelectorAll('.content');
+var counter = 0;
+slides.forEach( 
+    (box_container, index ) => {
+    box_container.style.left = `${index * 100}%`;
+});
+
+const goPrev = () => {
+    counter--;
+    if ( counter === -1 ) {
+        counter = slides.length;
+    } else {
+        slideImages();
+    }
+}
+const goNext = () => {
+    counter++;
+    if(counter === slides.length){
+        counter = -1;
+    } else {
+        slideImages();
+    }
+}
+
+const slideImages = () => {
+    slides.forEach( (box_container) => {
+        box_container.style.transform = `translateX(-${counter * 100}% )`;
+    })
+}
